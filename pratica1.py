cdb00fd40965dbc5e7b9b7183366d6f3172af3ff
Gabriel Pelizzaro <53914594+fullzer4@users.noreply.github.com>
2022-06-21 22:12:21 -0300

    att

--- pratica1.py
+++ pratica1.py
@@ -5,4 +5,18 @@ import matplotlib.pyplot as plt
 import plotly.express as px
 
 base_credit = pd.read_csv("./dados/credit_data.csv")
-print(base_credit)
+base_credit.tail(10)
+#head - tail (primeiros - ultimos)
+base_credit.describe()
+#retorna
+#contagem
+#media
+#desvio padrao
+#minimo (menos)
+#25%
+#50%
+#75%
+#maximo (mais)
+base_credit[base_credit['income'] >= 69995.685578] #pegar o usuario/s com o valor x (filtro por coluna)
+
+print(np.unique(base_credit['default'], return_counts=True))
